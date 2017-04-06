ef5cb0294073028465945669d2399a9b03b67718
thiolliere <guillaume.thiolliere@opmbx.org>
2017-04-06 23:57:37 +0200

    add catalog

---
+++ src/catalog.py
@@ -0,0 +1,25 @@
+import wget
+import pretty_print
+import os
+
+catalog_dir = "catalog"
+
+catalog_url_path = "http://catalog.ideascube.org"
+
+catalog_files = [
+        "kiwix.yml",
+        "static-sites.yml",
+        "bibliotecamovil.yml",
+        ]
+
+def make():
+    pretty_print.step("build zim catalog")
+    if os.path.isdir(catalog_dir):
+        pretty_print.std("nothing to do")
+        return
+
+    os.mkdir(catalog_dir)
+    for catalog in catalog_files:
+        wget.download("{}/{}".format(catalog_url_path, catalog), out="{}/{}".format(catalog_dir, catalog))
+        print()
+
