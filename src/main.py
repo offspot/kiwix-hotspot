dc2052a020a60d0f73869decc7fe5cd6d29912fe
thiolliere <guillaume.thiolliere@opmbx.org>
2017-04-08 21:15:24 +0200

    code restructuration

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ import argparse
 import vexpress_boot
 import catalog
 import raspbian
-import json
-import systemd
 import pretty_print
 import qemu
 
@@ -36,148 +34,22 @@ if args.catalog:
 vexpress_boot.make()
 raspbian.make()
 
-current_size = qemu.get_image_size(raspbian.image)
-resize_image = args.resize and args.resize != current_size
+emulator = qemu.Emulator(vexpress_boot.kernel_path, vexpress_boot.dtb_path, raspbian.image)
 
-if resize_image:
-    pretty_print.step("resize image")
-    qemu.resize_image(raspbian.image, current_size, args.resize)
+if args.resize:
+    emulator.resize_image(args.resize)
 
-vm = qemu.Qemu(vexpress_boot.kernel_path, vexpress_boot.dtb_path, raspbian.image)
-
-if resize_image:
-    pretty_print.step("resize partition")
-
-    # d  delete partition
-    # 2  second partition
-    # n  create partition
-    # p  primary partition
-    # 2  second partition
-    # %  start of partition
-    #    resize to max
-    # w  write change
-    fdiskCmd = """sudo LANG=C fdisk /dev/mmcblk0 <<END_OF_CMD
-d
-2
-n
-p
-2
-%d
-
-w
-END_OF_CMD""" % raspbian.secondPartitionSector
-    vm.exec(fdiskCmd)
-    vm.reboot()
-
-    pretty_print.step("resize filesystem")
-    vm.exec("sudo resize2fs /dev/mmcblk0p2")
-
-pretty_print.step("install ansible")
-
-# Use cat and then move because `sudo cat` doesn't give priviledge on redirection
-vm.exec("cat > /tmp/system.conf <<END_OF_CMD\n{}\nEND_OF_CMD".format(systemd.system_conf))
-vm.exec("sudo mv /tmp/system.conf /etc/systemd/system.conf")
-
-# Use cat and then move because `sudo cat` doesn't give priviledge on redirection
-vm.exec("cat > /tmp/user.conf <<END_OF_CMD\n{}\nEND_OF_CMD".format(systemd.user_conf))
-vm.exec("sudo mv /tmp/user.conf /etc/systemd/user.conf")
-
-vm.exec("sudo apt-get update")
-vm.exec("sudo apt-get install -y python-pip git python-dev libffi-dev libssl-dev gnutls-bin")
-
-vm.exec("sudo pip install ansible==2.2.2 markupsafe")
-vm.exec("sudo pip install cryptography --upgrade")
-
-pretty_print.step("clone ansiblecube")
-
-ansiblecube_url = "https://github.com/thiolliere/ansiblecube.git"
-ansiblecube_path = "/var/lib/ansible/local"
-
-vm.exec("sudo mkdir --mode 0755 -p %s" % ansiblecube_path)
-vm.exec("sudo git clone {url} {path}".format(url=ansiblecube_url, path=ansiblecube_path))
-vm.exec("sudo mkdir --mode 0755 -p /etc/ansible")
-vm.exec("sudo cp %s/hosts /etc/ansible/hosts" % ansiblecube_path)
-
-hostname = args.name.replace("_", "-")
-
-vm.exec("sudo hostname %s" % hostname)
-
-device_list = {hostname: {
-    "kalite": {
-        "activated": args.kalite != None,
-        "version": "0.16.9",
-        "language": args.kalite or [],
-    },
-    "idc_import": {
-        "activated": False,
-        "content_name": [],
-    },
-    "zim_install": {
-        "activated": args.zim_install != None,
-        "name": args.zim_install or [],
-    },
-    "portal": {
-        "activated": True, # TODO set portal activated
-    }
-}}
-
-facts_path = "/etc/ansible/facts.d"
-
-vm.exec("sudo mkdir --mode 0755 -p %s" % facts_path)
-
-# Use cat and then move because `sudo cat` doesn't give priviledge on redirection
-vm.exec("cat > /tmp/device_list.fact <<END_OF_CMD\n{}\nEND_OF_CMD".format(json.dumps(device_list, indent=4)))
-vm.exec("sudo mv /tmp/device_list.fact {}/device_list.fact".format(facts_path))
-
-extra_vars = "ideascube_project_name=%s" % args.name
-extra_vars += " timezone=%s" % args.timezone
-if args.wifi_pwd:
-    extra_vars += " wpa_pass=%s" % args.wifi_pwd
-extra_vars += " git_branch=oneUpdateFile"
-extra_vars += " own_config_file=True"
-extra_vars += " managed_by_bsf=False"
-
-ansible_pull_cmd = "sudo /usr/local/bin/ansible-pull"
-ansible_pull_cmd += " --checkout oneUpdateFile"
-ansible_pull_cmd += " --directory /var/lib/ansible/local"
-ansible_pull_cmd += " --inventory hosts"
-ansible_pull_cmd += " --url https://github.com/thiolliere/ansiblecube.git"
-ansible_pull_cmd += " --tags master,custom"
-ansible_pull_cmd += " --extra-vars \"%s\"" % extra_vars
-ansible_pull_cmd += " main.yml"
-
-vm.exec(ansible_pull_cmd)
-
-vm.close()
+with emulator.run() as emulation:
+    emulation.resize_fs()
+    emulation.run_ansible(
+            name=args.name,
+            timezone=args.timezone,
+            wifi_pwd=args.wifi_pwd,
+            kalite=args.kalite,
+            zim_install=args.zim_install)
 
 pretty_print.step("image done")
 
 if args.sd:
-    pretty_print.step("copy image to sd card")
-    qemu.get_image_size(raspbian.image)
-
-    image = os.open(raspbian.image, os.O_RDONLY)
-    sd = os.open(args.sd, os.O_WRONLY)
-
-    total_size = os.lseek(image, 0, os.SEEK_END)
-    os.lseek(image, 0, os.SEEK_SET)
-    current_percentage = 0.0
-    while True:
-        current_size = os.lseek(image, 0, os.SEEK_CUR)
-        new_percentage = (100 * current_size) / total_size
-        if new_percentage != current_percentage:
-            current_percentage = new_percentage
-            pretty_print.std(str(current_percentage) + "%")
-
-        buf = os.read(image, 4096)
-        if buf == b"":
-            break
-        os.write(sd, buf)
-
-    os.close(image)
-    pretty_print.step("sync")
-    os.fsync(sd)
-    os.close(sd)
-
-    pretty_print.step("done")
+    emulator.copy_image(args.sd)
 
--- src/qemu.py
+++ src/qemu.py
@@ -6,7 +6,10 @@ import socket
 import paramiko
 import select
 import re
+import json
+import raspbian
 import pretty_print
+import systemd
 from select import select
 
 def get_free_port():
@@ -16,6 +19,7 @@ def get_free_port():
 
     return port
 
+# TODO raise excpetion instead of return bool
 def wait_signal(fd, signal, timeout):
     ring_buf = collections.deque(maxlen=len(signal))
     while True:
@@ -34,31 +38,10 @@ def wait_signal(fd, signal, timeout):
 
 timeout = 60*3
 
-def get_image_size(image):
-    pipe_reader, pipe_writer = os.pipe()
-    subprocess.check_call(["qemu-img", "info", "-f", "raw", image], stdout=pipe_writer)
-    pipe_reader = os.fdopen(pipe_reader)
-    pipe_reader.readline()
-    pipe_reader.readline()
-    size_line = pipe_reader.readline()
-    matches = re.findall(r"virtual size: (.*)G", size_line)
-    assert(len(matches) == 1)
-    return float(matches[0])
-
-def resize_image(image, current_size, resize_size):
-    if resize_size < current_size:
-        pretty_print.err("error: cannot decrease image size")
-        exit(1)
-
-    subprocess.check_call(["qemu-img", "resize", "-f", "raw", image, "{}G".format(resize_size)])
-
-class Qemu:
+class Emulator:
     _image = None
     _kernel = None
     _dtb = None
-
-    _qemu = None
-    _client = None
 
     # login=pi
     # password=raspberry
@@ -69,8 +52,72 @@ class Qemu:
         self._dtb = dtb
         self._image = image
 
-    def _boot(self):
+    def run(self):
+        return _RunningInstance(self)
 
+    def _get_image_size(self):
+        pipe_reader, pipe_writer = os.pipe()
+        subprocess.check_call(["qemu-img", "info", "-f", "raw", self._image], stdout=pipe_writer)
+        pipe_reader = os.fdopen(pipe_reader)
+        pipe_reader.readline()
+        pipe_reader.readline()
+        size_line = pipe_reader.readline()
+        matches = re.findall(r"virtual size: (.*)G", size_line)
+        assert(len(matches) == 1)
+        return float(matches[0])
+
+    def resize_image(self, size):
+        if size < self._get_image_size():
+            pretty_print.err("error: cannot decrease image size")
+            exit(1)
+        subprocess.check_call(["qemu-img", "resize", "-f", "raw", self._image, "{}G".format(size)])
+
+    def copy_image(self, device_name):
+        pretty_print.step("copy image to sd card")
+
+        image = os.open(raspbian.image, os.O_RDONLY)
+        device = os.open(device_name, os.O_WRONLY)
+
+        total_size = os.lseek(image, 0, os.SEEK_END)
+        os.lseek(image, 0, os.SEEK_SET)
+
+        current_percentage = 0.0
+
+        while True:
+            current_size = os.lseek(image, 0, os.SEEK_CUR)
+            new_percentage = (100 * current_size) / total_size
+            if new_percentage != current_percentage:
+                current_percentage = new_percentage
+                pretty_print.std(str(current_percentage) + "%")
+
+            buf = os.read(image, 4096)
+            if buf == b"":
+                break
+            os.write(device, buf)
+
+        os.close(image)
+        pretty_print.step("sync")
+        os.fsync(device)
+        os.close(device)
+
+        pretty_print.step("done")
+
+class _RunningInstance:
+    _emulation = None
+    _qemu = None
+    _client = None
+
+    def __init__(self, emulation):
+        self._emulation = emulation
+
+    def __enter__(self):
+        self._boot()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._shutdown()
+
+    def _boot(self):
         ssh_port = get_free_port()
 
         stdout_reader, stdout_writer = os.pipe()
@@ -82,11 +129,11 @@ class Qemu:
             "qemu-system-arm",
             "-m", "1G",
             "-M", "vexpress-a9",
-            "-kernel", self._kernel,
-            "-dtb", self._dtb,
+            "-kernel", self._emulation._kernel,
+            "-dtb", self._emulation._dtb,
             "-append", "root=/dev/mmcblk0p2 console=ttyAMA0 console=tty",
             "-serial", "stdio",
-            "-sd", self._image,
+            "-sd", self._emulation._image,
             "-redir", "tcp:%d::22" % ssh_port,
             "-display", "none",
             "-no-reboot",
@@ -107,19 +154,11 @@ class Qemu:
         self._client.connect("localhost", port=ssh_port, username="pi", password="raspberry")
 
     def _shutdown(self):
-        self.exec("sudo shutdown 0")
+        self._exec_cmd("sudo shutdown 0")
         self._client.close()
         self._qemu.wait()
 
-        self._client = None
-        self._qemu = None
-
-    def reboot(self):
-        pretty_print.step("reboot qemu")
-        self._shutdown()
-        self._boot()
-
-    def exec(self, command):
+    def _exec_cmd(self, command):
         pretty_print.std(command)
         _, stdout, stderr = self._client.exec_command(command)
         while True:
@@ -131,7 +170,114 @@ class Qemu:
         for line in stderr.readlines():
             pretty_print.err("STDERR: " + line.replace("\n", ""))
 
-    def close(self):
-        pretty_print.step("shutdown")
+    def resize_fs(self):
+        pretty_print.step("resize partition")
+
+        # TODO compute secondPartitionSector here
+        # TODO if not needed then return, use qemu-img secteurs
+
+        # d  delete partition
+        # 2  second partition
+        # n  create partition
+        # p  primary partition
+        # 2  second partition
+        # %  start of partition
+        #    resize to max
+        # w  write change
+        fdiskCmd = """sudo LANG=C fdisk /dev/mmcblk0 <<END_OF_CMD
+d
+2
+n
+p
+2
+%d
+
+w
+END_OF_CMD""" % raspbian.secondPartitionSector
+        self._exec_cmd(fdiskCmd)
+        self._reboot()
+
+        pretty_print.step("resize filesystem")
+        self._exec_cmd("sudo resize2fs /dev/mmcblk0p2")
+
+    # TODO split in prepare and run
+    def run_ansible(self, name, timezone, wifi_pwd, kalite, zim_install):
+        # Use cat and then move because `sudo cat` doesn't give priviledge on redirection
+        self._exec_cmd("cat > /tmp/system.conf <<END_OF_CMD\n{}\nEND_OF_CMD".format(systemd.system_conf))
+        self._exec_cmd("sudo mv /tmp/system.conf /etc/systemd/system.conf")
+
+        # Use cat and then move because `sudo cat` doesn't give priviledge on redirection
+        self._exec_cmd("cat > /tmp/user.conf <<END_OF_CMD\n{}\nEND_OF_CMD".format(systemd.user_conf))
+        self._exec_cmd("sudo mv /tmp/user.conf /etc/systemd/user.conf")
+
+        self._exec_cmd("sudo apt-get update")
+        self._exec_cmd("sudo apt-get install -y python-pip git python-dev libffi-dev libssl-dev gnutls-bin")
+
+        self._exec_cmd("sudo pip install ansible==2.2.2 markupsafe")
+        self._exec_cmd("sudo pip install cryptography --upgrade")
+
+        pretty_print.step("clone ansiblecube")
+
+        ansiblecube_url = "https://github.com/thiolliere/ansiblecube.git"
+        ansiblecube_path = "/var/lib/ansible/local"
+
+        self._exec_cmd("sudo mkdir --mode 0755 -p %s" % ansiblecube_path)
+        self._exec_cmd("sudo git clone {url} {path}".format(url=ansiblecube_url, path=ansiblecube_path))
+        self._exec_cmd("sudo mkdir --mode 0755 -p /etc/ansible")
+        self._exec_cmd("sudo cp %s/hosts /etc/ansible/hosts" % ansiblecube_path)
+
+        hostname = name.replace("_", "-")
+
+        self._exec_cmd("sudo hostname %s" % hostname)
+
+        device_list = {hostname: {
+            "kalite": {
+                "activated": kalite != None,
+                "version": "0.16.9",
+                "language": kalite or [],
+            },
+            "idc_import": {
+                "activated": False,
+                "content_name": [],
+            },
+            "zim_install": {
+                "activated": zim_install != None,
+                "name": zim_install or [],
+            },
+            "portal": {
+                "activated": True,
+            }
+        }}
+
+        facts_path = "/etc/ansible/facts.d"
+
+        self._exec_cmd("sudo mkdir --mode 0755 -p %s" % facts_path)
+
+        # Use cat and then move because `sudo cat` doesn't give priviledge on redirection
+        self._exec_cmd("cat > /tmp/device_list.fact <<END_OF_CMD\n{}\nEND_OF_CMD".format(json.dumps(device_list, indent=4)))
+        self._exec_cmd("sudo mv /tmp/device_list.fact {}/device_list.fact".format(facts_path))
+
+        extra_vars = "ideascube_project_name=%s" % name
+        extra_vars += " timezone=%s" % timezone
+        if wifi_pwd:
+            extra_vars += " wpa_pass=%s" % wifi_pwd
+        extra_vars += " git_branch=oneUpdateFile"
+        extra_vars += " own_config_file=True"
+        extra_vars += " managed_by_bsf=False"
+
+        ansible_pull_cmd = "sudo /usr/local/bin/ansible-pull"
+        ansible_pull_cmd += " --checkout oneUpdateFile"
+        ansible_pull_cmd += " --directory /var/lib/ansible/local"
+        ansible_pull_cmd += " --inventory hosts"
+        ansible_pull_cmd += " --url https://github.com/thiolliere/ansiblecube.git"
+        ansible_pull_cmd += " --tags master,custom"
+        ansible_pull_cmd += " --extra-vars \"%s\"" % extra_vars
+        ansible_pull_cmd += " main.yml"
+
+        self._exec_cmd(ansible_pull_cmd)
+
+    def _reboot(self):
+        pretty_print.step("reboot qemu")
         self._shutdown()
+        self._boot()
 
