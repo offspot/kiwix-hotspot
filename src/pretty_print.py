d6fe0e55234a4f00e63e41c251797235d2ca7620
thiolliere <guillaume.thiolliere@opmbx.org>
2017-04-10 14:33:28 +0200

    reporthook: doesn't print if same line

--- src/pretty_print.py
+++ src/pretty_print.py
@@ -16,16 +16,22 @@ def newline():
 class ReportHook():
     _current_size = 0
     width = 80
+    _last_line = None
     def reporthook (self, chunk, chunk_size, total_size):
         if chunk != 0:
             self._current_size += chunk_size
 
         avail_dots = self.width-2
-        ratio = min(float(self._current_size) / total_size, 1.)
-        shaded_dots = min(int( ratio * avail_dots), avail_dots)
-        percent = min(int(ratio*100), 100)
-        sys.stdout.write("[" + "."*shaded_dots + " "*(avail_dots-shaded_dots) + "] " + str(percent) + "%\r")
         if self._current_size >= total_size:
-            sys.stdout.write("[" + "."*avail_dots + "] 100%\n")
+            line = "[" + "."*avail_dots + "] 100%\n"
+        else:
+            ratio = min(float(self._current_size) / total_size, 1.)
+            shaded_dots = min(int( ratio * avail_dots), avail_dots)
+            percent = min(int(ratio*100), 100)
+            line = "[" + "."*shaded_dots + " "*(avail_dots-shaded_dots) + "] " + str(percent) + "%\r"
+
+        if line != self._last_line:
+            self._last_line = line
+            sys.stdout.write(line)
 
 wget_bar = wget.bar_adaptive
