2e154d7727ae38c0ec4784734e81de517c5f3806
thiolliere <guillaume.thiolliere@opmbx.org>
2017-06-29 14:13:46 +0200

    delete unused download_vexpress_boot

--- pibox-installer/backend/downloads.py
+++ pibox-installer/backend/downloads.py
@@ -24,30 +24,6 @@ class Downloader:
                 shutil.move(extraction, path)
         os.remove(zip_filename)
 
-    def download_vexpress_boot(self):
-        boot_dir = "pibox-installer-vexpress-boot"
-
-        kernel_name = "zImage"
-        kernel_path = os.path.join(boot_dir, kernel_name)
-        kernel_zip_path = posixpath.join(boot_dir, kernel_name)
-
-        dtb_name = "vexpress-v2p-ca9.dtb"
-        dtb_path = os.path.join(boot_dir, dtb_name)
-        dtb_zip_path = posixpath.join(boot_dir, dtb_name)
-
-        url = "http://mirror.download.kiwix.org/dev/" + boot_dir + ".zip"
-
-        self._logger.step("get vexpress boot")
-        os.makedirs(boot_dir, exist_ok=True)
-        if os.path.isfile(kernel_path) and os.path.isfile(dtb_path):
-            self._logger.std("nothing to do")
-        else:
-            content = [(kernel_zip_path, kernel_path),
-                       (dtb_zip_path, dtb_path)]
-            self._download_and_extract(url, content)
-
-        return kernel_path, dtb_path
-
     def download_raspbian(self):
         version = "2017-03-02"
         image_path = version + "-raspbian-jessie-lite.img"
