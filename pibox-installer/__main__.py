f911e8cff0e5b127651c36f5caa8bd8a1dc5d8ee
thiolliere <guillaume.thiolliere@opmbx.org>
2017-08-16 14:02:23 -0400

    run cli from main in subcommand

--- pibox-installer/__main__.py
+++ pibox-installer/__main__.py
@@ -1,1 +1,13 @@
-import gui
+import argparse
+import sys
+import runpy
+
+if len(sys.argv) == 1:
+    runpy.run_module("gui")
+elif sys.argv[1] == "cli":
+    sys.argv.pop(1)
+    runpy.run_module("cli")
+else:
+    parser = argparse.ArgumentParser(description="ideascube/kiwix installer for raspberrypi.")
+    parser.add_subparsers().add_parser("cli", help="run CLI mode")
+    parser.parse_args()
