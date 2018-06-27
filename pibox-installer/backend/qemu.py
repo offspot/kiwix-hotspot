c510aa2681071227780523b7f0b05bc4302c2d52
renaud gaudin <rgaudin@gmail.com>
2018-06-27 16:16:38 +0000

    updated qemu backend
    	dynamically assigns cpus/cores based on the those available
    	RAM usage can be specified and is adjusted if out of bounds
    	moved `qemu-img info` command to a dedicated function as it's reused when mounting
    	qemu parameters are updated to the recomended syntax
    	file system resize is removed as now handled inside ansiblecube

--- pibox-installer/backend/qemu.py
+++ pibox-installer/backend/qemu.py
@@ -9,6 +9,9 @@ import random
 import time
 import threading
 import posixpath
+import psutil
+import multiprocessing
+from util import ONE_GiB, ONE_MiB, human_readable_size
 from .util import startup_info_args
 from .util import subprocess_pretty_check_call
 
@@ -21,13 +24,16 @@ else:
     qemu_system_arm_exe = "qemu-system-arm"
     qemu_img_exe = "qemu-img"
 
-if getattr(sys, "frozen", False):
-    bin_path = sys._MEIPASS
-else:
-    bin_path = ""
+bin_path = sys._MEIPASS if getattr(sys, "frozen", False) else "."
 
 qemu_system_arm_exe_path = os.path.join(bin_path, qemu_system_arm_exe)
 qemu_img_exe_path = os.path.join(bin_path, qemu_img_exe)
+nb_cpus = multiprocessing.cpu_count()
+qemu_cpu = nb_cpus - 1 if nb_cpus >= 2 else nb_cpus
+# vexpress-a15 is limited to 4 cores
+if qemu_cpu > 4:
+    qemu_cpu = 4
+host_ram = int(psutil.virtual_memory().total)
 
 class QemuException(Exception):
     def __init__(self, msg):
@@ -46,6 +52,17 @@ def get_free_port():
 
     return port
 
+def get_qemu_image_size(image_fpath, logger):
+    output = subprocess_pretty_check_call([qemu_img_exe_path, "info", "-f", "raw", image_fpath], logger)
+    matches = []
+    for line_number, line in enumerate(output):
+        if line_number == 2:
+            matches = re.findall(b"virtual size: \S*G \((\d*) bytes\)", line)
+
+    if len(matches) != 1:
+        raise QemuException("cannot get image %s size from qemu-img info" % image_fpath)
+    return int(matches[0])
+
 class Emulator:
     _image = None
     _kernel = None
@@ -56,25 +73,50 @@ class Emulator:
     # password=raspberry
     # prompt end by ":~$ "
     # sudo doesn't require password
-    def __init__(self, kernel, dtb, image, logger):
+    def __init__(self, kernel, dtb, image, logger, ram):
         self._kernel = kernel
         self._dtb = dtb
         self._image = image
         self._logger = logger
+        self._binary = qemu_system_arm_exe_path
+        self._set_ram(ram.lower())
+
+    def _set_ram(self, requested_ram):
+        ''' applies requested RAM to qemu if it's available otherwise less '''
+
+        # less than a GB is very short
+        if host_ram / ONE_GiB <= 1.0:
+            self._ram = '256m'
+            return
+
+        # at most, use RAM minus 512m
+        max_ram = int(host_ram - ONE_GiB / 2)
+
+        # parse specified ram in Mega or Giga
+        if re.match(r'\d+[mg]$', requested_ram):
+            ram_amount, ram_unit = int(requested_ram[:-1]), requested_ram[-1]
+        else:
+            # no unit, assuming M
+            ram_amount, ram_unit = int(requested_ram), 'm'
+        if ram_unit == 'g':
+            ram_amount = ram_amount * (ONE_GiB)
+
+        # use requested if it doesn't exceed max_ram
+        ram = max_ram if ram_amount > max_ram else ram_amount
+
+        # vexpress-a15 is capped at 30G
+        if int(ram / ONE_GiB) > 30:
+            ram = 30 * ONE_GiB
+
+        self._ram = "{ram}M".format(ram=int(ram / ONE_MiB))
+        self._logger.std(" using {ram} RAM"
+                         .format(ram=human_readable_size(ram)))
 
     def run(self, cancel_event):
         return _RunningInstance(self, self._logger, cancel_event)
 
     def get_image_size(self):
-        output = subprocess_pretty_check_call([qemu_img_exe_path, "info", "-f", "raw", self._image], self._logger)
-        matches = []
-        for line_number, line in enumerate(output):
-            if line_number == 2:
-                matches = re.findall(b"virtual size: \S*G \((\d*) bytes\)", line)
-
-        if len(matches) != 1:
-            raise QemuException("cannot get image %s size from qemu-img info" % self._image)
-        return int(matches[0])
+        return get_qemu_image_size(self._image, self._logger)
 
     def resize_image(self, size):
         subprocess_pretty_check_call([qemu_img_exe_path, "resize", "-f", "raw", self._image, "{}".format(size)], self._logger)
@@ -102,6 +144,7 @@ class Emulator:
             new_percentage = (100 * current_size) / total_size
             if new_percentage != current_percentage:
                 current_percentage = new_percentage
+                self._logger.progress(current_percentage / 100)
                 self._logger.std(str(current_percentage) + "%\r")
 
             buf = os.read(image, 4096)
@@ -109,6 +152,7 @@ class Emulator:
                 break
             os.write(device, buf)
 
+        self._logger.progress(1)
         os.close(image)
         self._logger.step("Sync")
         os.fsync(device)
@@ -191,22 +235,31 @@ class _RunningInstance:
         self._logger.std("ssh on port {}".format(ssh_port))
 
         with self._cancel_event.lock() as cancel_register:
-            self._qemu = subprocess.Popen([
-                qemu_system_arm_exe_path,
-                "-m", "2G",
+            command = [
+                self._emulation._binary,
+                "-m", self._emulation._ram,
                 "-M", "vexpress-a15",
                 "-kernel", self._emulation._kernel,
                 "-dtb", self._emulation._dtb,
                 "-append", "root=/dev/mmcblk0p2 console=ttyAMA0 console=tty",
                 "-serial", "stdio",
-                "-sd", self._emulation._image,
-                "-redir", "tcp:%d::22" % ssh_port,
+                "-no-acpi",
+                "-drive", "format=raw,if=sd,file={}"
+                .format(self._emulation._image),
                 "-display", "none",
                 "-no-reboot",
-                "-smp", "2",
-                "--accel", "tcg,thread=multi",
-                "-no-acpi",
-                ], stdin=stdin_reader, stdout=stdout_writer, stderr=subprocess.STDOUT, **startup_info_args())
+                "-netdev", "user,id=eth1,hostfwd=tcp::{}-:22".format(ssh_port),
+                "-device",
+                "virtio-net-device,netdev=eth1",
+            ]
+            if qemu_cpu > 1:
+                command += ["-smp", str(qemu_cpu),
+                            "--accel", "tcg,thread=multi"]
+            self._logger.std("--\n{}\n--".format(" ".join(command)))
+            self._qemu = subprocess.Popen(
+                command,
+                stdin=stdin_reader, stdout=stdout_writer,
+                stderr=subprocess.STDOUT, **startup_info_args())
             cancel_register.register(self._qemu.pid)
 
         self._wait_signal(stdout_reader, stdout_writer, b"login: ", timeout)
@@ -246,12 +299,17 @@ class _RunningInstance:
                              allow_agent=False, look_for_keys=False)
 
     def _shutdown(self):
-        self.exec_cmd("sudo shutdown -P 0")
+        self.exec_cmd("sudo sync")
+        self.exec_cmd("sudo shutdown -P 0", check=False)
         self._client.close()
-        self._qemu.wait(timeout)
+        try:
+            self._qemu.wait(timeout)
+        except subprocess.TimeoutExpired:
+            pass
         with self._cancel_event.lock() as cancel_register:
             cancel_register.unregister(self._qemu.pid)
         self._qemu = None
+        self._logger.std("VM is off.")
 
     # The remote path must not exist
     def put_dir(self, localpath, remotepath):
@@ -278,7 +336,8 @@ class _RunningInstance:
                 sftp_client.mkdir(dir_remote_path)
 
             for filename in filenames:
-                file_local_path = os.path.join(localpath, localdirpath, filename)
+                file_local_path = os.path.normpath(
+                    os.path.join(localdirpath, filename))
                 file_remote_path = posixpath.join(tmpremotepath, remotedirpath, filename)
                 self._logger.std("copy local file {} to tmp file {}".format(file_local_path, file_remote_path))
                 sftp_client.put(file_local_path, file_remote_path)
@@ -313,7 +372,7 @@ class _RunningInstance:
                 break
             if capture_stdout:
                 stdout_buffer += line
-            self._logger.std(line.replace("\n", ""))
+            self._logger.ansible(line.replace("\n", ""))
 
         for line in stderr.readlines():
             self._logger.err("STDERR: " + line.replace("\n", ""))
@@ -325,58 +384,7 @@ class _RunningInstance:
         if capture_stdout:
             return stdout_buffer
 
-    def resize_fs(self):
-        self._logger.step("Resize partition")
-
-        stdout = self.exec_cmd("sudo LANG=C fdisk -l /dev/mmcblk0", capture_stdout=True)
-
-        lines = stdout.splitlines()
-
-        number_of_sector_match = []
-        second_partition_match = []
-        for line in lines:
-            number_of_sector_match += re.findall(r"^Disk /dev/mmcblk0:.*, (\d+) sectors$", line)
-            second_partition_match += re.findall(r"^/dev/mmcblk0p2 +(\d+) +(\d+) +\d+ +\S+ +\d+ +Linux$", line)
-
-        if len(number_of_sector_match) != 1:
-            raise QemuException("cannot find the number of sector of disk")
-        number_of_sector = int(number_of_sector_match[0])
-
-        if len(second_partition_match) != 1:
-            raise QemuException("cannot find start and/or end of root partition of disk")
-        second_partition_start = int(second_partition_match[0][0])
-        second_partition_end = int(second_partition_match[0][1])
-
-        if second_partition_end + 1 == number_of_sector:
-            self._logger.std("nothing to do")
-        else:
-
-            # d  delete partition
-            # 2  second partition
-            # n  create partition
-            # p  primary partition
-            # 2  second partition
-            # %  start of partition
-            #    resize to max
-            # w  write change
-            fdiskCmd = """sudo LANG=C fdisk /dev/mmcblk0 <<END_OF_CMD
-d
-2
-n
-p
-2
-%d
-
-w
-END_OF_CMD""" % second_partition_start
-            self.exec_cmd(fdiskCmd, check=False)
-            self._reboot()
-
-        self._logger.step("Resize filesystem")
-        self.exec_cmd("sudo resize2fs /dev/mmcblk0p2")
-
     def _reboot(self):
         self._logger.std("reboot qemu")
         self._shutdown()
         self._boot()
-
