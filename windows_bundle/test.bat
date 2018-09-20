89015d8ee3f10ab62837cff536fcd133cf2cb82a
renaud gaudin <rgaudin@gmail.com>
2018-09-20 15:20:40 +0200

    all in bat

--- windows_bundle/test.bat
+++ windows_bundle/test.bat
@@ -2,5 +2,7 @@
 echo "running test bat"
 C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -help -log CONSOLE
 C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -open .\resources.rc -save .\resources.res -action compile -log rh.txt
+C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -open kiwix-hotspot.exe -save kiwix-hotspot.exe -action addoverwrite -res resources.res -log rh.txt
+C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -save kiwix-hotspot.exe -action addskip -res icon.ico -mask ICONGROUP,MAINICON, -log rh.txt
 type rh.txt
 echo "done bat"
