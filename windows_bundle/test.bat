4d1d41960b65ec3cae0297b8dcde6cadb42f827d
renaud gaudin <rgaudin@gmail.com>
2018-09-20 15:11:13 +0200

    no log for help

--- windows_bundle/test.bat
+++ windows_bundle/test.bat
@@ -1,7 +1,6 @@
 
 echo "running test bat"
-C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -help
-type resourcehacker.log
-C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -open .\resources.rc -save .\resources.res -action compile
-type resourcehacker.log
+C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -help -log CONSOLE
+C:\ProgramData\chocolatey\lib\reshack.portable\tools\ResourceHacker.exe -open .\resources.rc -save .\resources.res -action compile -log rh.txt
+type rh.txt
 echo "done bat"
