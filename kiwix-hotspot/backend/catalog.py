f4f293bdc170403ce3ffc4541e17b27e83337253
renaud gaudin <rgaudin@gmail.com>
2018-10-24 16:38:19 +0000

    Removed StaticSites catalog as it is not available anymore online

--- kiwix-hotspot/backend/catalog.py
+++ kiwix-hotspot/backend/catalog.py
@@ -12,12 +12,7 @@ CATALOGS = [
         "name": "Kiwix",
         "description": "Kiwix ZIM Content",
         "url": "http://download.kiwix.org/library/ideascube.yml",
-    },
-    {
-        "name": "StaticSites",
-        "description": "Static sites",
-        "url": "http://catalog.ideascube.org/static-sites.yml",
-    },
+    }
 ]
 
 YAML_CATALOGS = None
